7399f3024ab4e8a59efedb440662e1f6a4bfa2f9
Thati <ahthati@deloitte.com>
2026-07-28 15:49:31 +0530

    updateRunner updated TestRunner

--- src/test/java/com/ixigo/travelbooking/runner/TestRunner.java
+++ src/test/java/com/ixigo/travelbooking/runner/TestRunner.java
@@ -11,11 +11,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-       		features = {//"src/test/resources/features/featuretorun.feature",
-       				"src/test/resources/features/buses.feature",
-       				"src/test/resources/features/flights.feature",
-       				"src/test/resources/features/hotels.feature",
-       				"src/test/resources/features/login.feature",
+       		features = {"src/test/resources/features/featuretorun.feature",
+       				//"src/test/resources/features/buses.feature",
+       				//"src/test/resources/features/flights.feature",
+       				//"src/test/resources/features/hotels.feature",
+       				//"src/test/resources/features/login.feature",
        		},
        		
     glue = {
